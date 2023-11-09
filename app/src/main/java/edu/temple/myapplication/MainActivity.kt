b9b7875b6b9639d9e455077510edb40f8ec5bcc7
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2023-11-09 14:55:03 -0500

    Added a handler so that the timer is now displayed in MainActivity's layout, rather than just displaying in the log

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,21 +5,34 @@ import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var timerTextView: TextView
+
     lateinit var timerBinder : TimerService.TimerBinder
     var isConnected = false
+
+    val timerHandler = Handler(Looper.getMainLooper()) {
+        timerTextView.text = it.what.toString()
+        true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        timerTextView = findViewById(R.id.timerTextView)
+
         val serviceConnection = object : ServiceConnection {
             override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
                 timerBinder = service as TimerService.TimerBinder
+                timerBinder.setHandler(timerHandler)
                 isConnected = true
             }
 
@@ -37,7 +50,7 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
             if (isConnected) {
-                timerBinder.start(10)
+                timerBinder.start(60)
             }
         }
 
--- app/src/main/java/edu/temple/myapplication/TimerService.kt
+++ app/src/main/java/edu/temple/myapplication/TimerService.kt
@@ -3,11 +3,14 @@ package edu.temple.myapplication
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
+import android.os.Handler
 import android.os.IBinder
 import android.util.Log
 
 @Suppress("ControlFlowWithEmptyBody")
 class TimerService : Service() {
+
+    lateinit var timerHandler : Handler
 
     private var isRunning = false
 
@@ -51,6 +54,10 @@ class TimerService : Service() {
             return this@TimerService
         }
 
+        fun setHandler(handler: Handler) {
+            timerHandler = handler
+        }
+
     }
 
     override fun onCreate() {
@@ -85,6 +92,10 @@ class TimerService : Service() {
 
                         while (paused);
                         sleep(1000)
+
+                        if (::timerHandler.isInitialized) {
+                            timerHandler.sendEmptyMessage(i)
+                        }
 
                 }
                 isRunning = false
